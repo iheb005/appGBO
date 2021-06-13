9e58f4c69e0d5210efd71d7f211415fc884ae37f
eyaboulaabi <79916226+eyaboulaabi@users.noreply.github.com>
2021-06-13 16:22:22 +0200

    commit

--- pfe-back-end/src/main/java/com/example/demo/repository/StructureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.Structure;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StructureRepository extends JpaRepository<Structure, Long> {
-}
--- pfe-back-end/src/main/java/com/example/demo/repository/UtilisateurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.Utilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
-
-    Utilisateur findByEmail(String email);
-}
